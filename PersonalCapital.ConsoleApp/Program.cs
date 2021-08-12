2c7e7607965b10d2653926b624da1ff155718e1e
Patrick Cartwright <pcartwright1981@gmail.com>
2021-08-12 17:42:07 -0500

    Clean up and refactor

--- PersonalCapital.ConsoleApp/Program.cs
+++ PersonalCapital.ConsoleApp/Program.cs
@@ -1,122 +1,173 @@
-﻿using PersonalCapital.Api;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using PersonalCapital.Api;
 using PersonalCapital.Exceptions;
+using PersonalCapital.Request;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using PersonalCapital.Request;
+using System.Threading.Tasks;
 using TestApplication.Extensions;
 
-namespace TestApplication {
-    internal class Program {
-        private static void Main()
+namespace TestApplication
+{
+    internal class Program
+    {
+        private static void Initialize()
         {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(
+                    (hostingContext, config) => { })
+                .ConfigureServices(
+                    (ctx, services) =>
+                    {
+                        services.AddLogging(
+                            opt =>
+                            {
+                                opt.AddSimpleConsole(
+                                    c =>
+                                    {
+                                        c.TimestampFormat = "[HH:mm:ss] ";
+                                    });
+                            });
+                    })
+                .Build();
+            using var serviceScope = host.Services.CreateScope();
+            var provider = serviceScope.ServiceProvider;
+            logger = provider.GetRequiredService<ILogger<Program>>();
+        }
+
+        private static async Task Main()
+        {
+            Initialize();
             var file = "sessionFile";
             // Get username from settings
             var username = Environment.GetEnvironmentVariable("PEW_EMAIL");
-            if (username == null) {
-                Console.WriteLine("Set the username in the appSettings section of the app.config");
+            if(username == null)
+            {
+                logger.LogError("Set the username in the appSettings section of the app.config");
                 Pause();
                 return;
             }
 
             // Get password from vault
             var password = Environment.GetEnvironmentVariable("PEW_PASSWORD");
-            if (password == null) {
+            if(password == null)
+            {
                 // Get password from the console
                 Console.Write("Password: ");
                 password = Console.ReadLine();
                 Console.Clear();
             }
 
-            Console.WriteLine("Attempting log in");
+            logger.LogInformation("Attempting log in");
 
-            using (var pcClient = new PersonalCapitalClient()) {
-                try {
+            using(var pcClient = new PersonalCapitalClient())
+            {
+                try
+                {
                     // Restore session from the file configured if it exists
-                    if (File.Exists(file)) {
-                        Console.WriteLine($"Restoring session from {file}");
+                    if(File.Exists(file))
+                    {
+                        logger.LogInformation($"Restoring session from {file}");
                         pcClient.RestoreSession(file);
                     }
                     // Attempt to login; successful if no exceptions are thrown
-                    pcClient.Login(username, password).GetAwaiter().GetResult();
-                    Console.WriteLine("Logged in Successfully");
-                }
-                catch (RequireTwoFactorException) {
-                    pcClient.SendTwoFactorChallenge(TwoFactorVerificationMode.SMS).GetAwaiter().GetResult();
+                    await pcClient.Login(username, password);
+                    logger.LogInformation("Logged in Successfully");
+                } catch(RequireTwoFactorException)
+                {
+                    await pcClient.SendTwoFactorChallenge(TwoFactorVerificationMode.SMS);
                     string code;
-                    do {
+                    do
+                    {
                         Console.Write("Code: ");
                         code = Console.ReadLine();
-                        if (string.IsNullOrEmpty(code)) {
+                        if(string.IsNullOrEmpty(code))
+                        {
                             break;
                         }
-                        var result = pcClient.TwoFactorAuthenticate(TwoFactorVerificationMode.SMS, code).GetAwaiter().GetResult();
-                        if (result.Header.AuthLevel == Constants.AuthLevel.DeviceAuthorized) {
+                        var result = await pcClient.TwoFactorAuthenticate(TwoFactorVerificationMode.SMS, code);
+                        if(result.Header.AuthLevel == Constants.AuthLevel.DeviceAuthorized)
+                        {
                             break;
                         }
-                    } while (!string.IsNullOrEmpty(code));
-                    if (!string.IsNullOrEmpty(code)) {
+                    }
+                    while (!string.IsNullOrEmpty(code));
+                    if(!string.IsNullOrEmpty(code))
+                    {
                         // Challenge passed successfully
-                        var result = pcClient.AuthenticatePassword(password).GetAwaiter().GetResult();
-                        if (result.Header.AuthLevel == Constants.AuthLevel.SessionAuthenticated) {
-                            Console.WriteLine("Logged in Successfully");
+                        var result = await pcClient.AuthenticatePassword(password);
+                        if(result.Header.AuthLevel == Constants.AuthLevel.SessionAuthenticated)
+                        {
+                            logger.LogInformation("Logged in Successfully");
                             pcClient.PersistSession(file);
                         }
                     }
-                    else {
-                        Console.WriteLine("Two factor authentication failed. Unable to log in.");
+                    else
+                    {
+                        logger.LogError("Two factor authentication failed. Unable to log in.");
                         Pause();
                         return;
                     }
-                }
-                catch (PersonalCapitalException e) {
-                    Console.Write($"{e.GetType().Name}: ");
-                    Console.WriteLine(e.Message);
+                } 
+                catch(PersonalCapitalException e)
+                {                   
+                    logger.LogError(e.Message);
                     Pause();
                     return;
                 }
 
-                var bills = pcClient.FetchBills().GetAwaiter().GetResult();
-                //var updateTransactionRequest = new UpdateTransactionRequest(new List<long>{9334839086}, 
-                //    "Vanguard 529", 
-                //    54, 
-                //    false, 
-                //    "");
-                //var x = pcClient.UpdateTransaction(updateTransactionRequest).GetAwaiter().GetResult();
-                //if (x.Data.Count > 0)
-                //{
-                //    Console.WriteLine("Updated Transaction Success");
-                //}
-                var usermessage = pcClient.FetchUserMessages().GetAwaiter().GetResult();
-                usermessage.Data.UserMessages.Select(c=>c.Summary).ToList().ForEach(Console.WriteLine);
-                var accountResponse = pcClient.FetchAccounts().GetAwaiter().GetResult();
-                Console.WriteLine($"Net Worth: {accountResponse.Data.Networth}");
-                foreach (var group in accountResponse.Data.Accounts.GroupBy(x=>x.ProductType)) {
-                    Console.WriteLine(group.Key?.Replace('_',' ') + ":");
-                    foreach (var account in group) {
-                        Console.WriteLine($"    {account.UserAccountId} -> {account.Name}: ${account.Balance}");
+                var bills = await pcClient.FetchBills();
+                var usermessage = await pcClient.FetchUserMessages();
+                var outputs = usermessage.Data.UserMessages.Select(c => c.Summary).ToList();
+                LogOutputs(outputs);
+                var accountResponse = await pcClient.FetchAccounts();
+                outputs = new List<string> 
+                {
+                    $"Net Worth: {accountResponse.Data.Networth}"
+                };               
+                foreach(var group in accountResponse.Data.Accounts.GroupBy(x => x.ProductType))
+                {
+                    outputs.Add((group.Key?.Replace('_', ' ')) + ":");
+                    foreach(var account in group)
+                    {
+                        outputs.Add($"{account.UserAccountId} -> {account.Name}: ${account.Balance}");
                     }
                 }
-                Console.WriteLine();
-                var transactionsResponse = pcClient.FetchUserTransactions(new FetchUserTransactionsRequest(DateTime.Today.AddDays(-7), DateTime.Today)).GetAwaiter().GetResult();
-                Console.WriteLine($"Net Cash Flow: {transactionsResponse.Data.NetCashflow}");
-                Console.WriteLine($"Date Range: {transactionsResponse.Data.StartDate} - {transactionsResponse.Data.EndDate}");
-                foreach (var tx in transactionsResponse.Data.Transactions.Where(x => x.IsSpending))
+                LogOutputs(outputs);
+                var transactionsResponse = await  pcClient.FetchUserTransactions(
+                    new FetchUserTransactionsRequest(DateTime.Today.AddDays(-7), DateTime.Today));
+                outputs = new List<string>
                 {
-                    Console.WriteLine($"{tx.SimpleDescription.WhenNullOrEmpty(tx.TransactionType)} : ${tx.Amount}");
+                    $"Net Cash Flow: {transactionsResponse.Data.NetCashflow}",
+                    $"Date Range: {transactionsResponse.Data.StartDate} - {transactionsResponse.Data.EndDate}"
+                };
+                
+                foreach(var tx in transactionsResponse.Data.Transactions.Where(x => x.IsSpending))
+                {
+                    outputs.Add($"{tx.SimpleDescription.WhenNullOrEmpty(tx.TransactionType)} : ${tx.Amount}");
                 }
+                LogOutputs(outputs);
             }
             Pause();
+        }
+
+        private static void LogOutputs(List<string> messages)
+        {
+            logger.LogInformation(string.Join(Environment.NewLine, messages.Where(x => !string.IsNullOrWhiteSpace(x))));
         }
 
         /// <summary>
         /// Implement the pause console command
         /// </summary>
-        private static void Pause() {
-            Console.WriteLine();
+        private static void Pause()
+        {
             Console.Write("Press any key to continue...");
             Console.ReadKey(true);
         }
+        private static ILogger<Program> logger;
     }
 }
